b252c09a31ecc0452e37afe74ff78c39380a6724
Developer From Jokela <developerfromjokela@gmail.com>
2023-01-28 10:15:58 +0200

    change in model

--- Sources/openwilma/Models/WilmaServer.swift
+++ Sources/openwilma/Models/WilmaServer.swift
@@ -10,4 +10,9 @@ import Foundation
 public struct WilmaServer: Codable, Hashable {
     public let url: String
     public var name: String? = nil
+    
+    public init(url: String, name: String? = nil) {
+        self.url = url
+        self.name = name
+    }
 }
