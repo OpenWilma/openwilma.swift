4371e6f59808402f92408dd3824e5694ff93f462
Developer From Jokela <developerfromjokela@gmail.com>
2023-01-29 10:38:06 +0200

    add: constructor for role

--- Sources/openwilma/Models/User/WilmaRole.swift
+++ Sources/openwilma/Models/User/WilmaRole.swift
@@ -15,4 +15,14 @@ public struct WilmaRole: Codable, Hashable {
     public var formKey: String?
     public var schools: [WilmaSchool]?
     public var profilePicture: String?
+    
+    public init(name: String, type: UserType, primusId: Int, slug: String? = nil, formKey: String? = nil, schools: [WilmaSchool]? = nil, profilePicture: String? = nil) {
+        self.name = name
+        self.type = type
+        self.primusId = primusId
+        self.slug = slug
+        self.formKey = formKey
+        self.schools = schools
+        self.profilePicture = profilePicture
+    }
 }
